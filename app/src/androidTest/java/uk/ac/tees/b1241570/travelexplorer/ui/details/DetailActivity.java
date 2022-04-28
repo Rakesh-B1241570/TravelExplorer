ba29de5faf66231038d2e42309ae1a2b50bb2d0a
Rakesh-B1241570 <rakesh.ga@yahoo.com>
2022-04-28 01:42:39 +0100

    Commit-28APR2022 - Sprint02-02

---
+++ app/src/androidTest/java/uk/ac/tees/b1241570/travelexplorer/ui/details/DetailActivity.java
@@ -0,0 +1,120 @@
+package uk.ac.tees.b1241570.travelexplorer.ui.details;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import studio.knowhere.travelappg.R;
+import studio.knowhere.travelappg.data.Data;
+import studio.knowhere.travelappg.data.DataManger;
+
+public class DetailActivity extends Activity implements View.OnClickListener {
+
+    private ImageView image;
+    private TextView city;
+    private TextView hotel;
+    private TextView date;
+    private TextView bed;
+    private TextView adult;
+    private TextView children;
+    private TextView address;
+    private ListView restaurentList;
+    private TextView changeLocation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail);
+
+        LinearLayout location = (LinearLayout) findViewById(R.id.location);
+        image = (ImageView) findViewById(R.id.image);
+        city = (TextView) findViewById(R.id.city);
+        hotel = (TextView) findViewById(R.id.hotel);
+        date = (TextView) findViewById(R.id.dates);
+        bed = (TextView) findViewById(R.id.bed);
+        adult = (TextView) findViewById(R.id.adult);
+        children = (TextView) findViewById(R.id.children);
+        restaurentList = (ListView) findViewById(R.id.restaurent_list);
+        address = (TextView) findViewById(R.id.address);
+        changeLocation = (TextView) findViewById(R.id.changeLocation);
+
+        location.setOnClickListener(this);
+
+        setValues(DataManger.getInstance().getSampleData()
+                .get(getIntent().getExtras().getString(getString(R.string.share_intent)))
+        );
+    }
+
+    private void setValues(Data data) {
+        image.setImageResource(data.getImage());
+        city.setText(data.getCity());
+        hotel.setText(data.getHotel());
+        hotel.append(getString(R.string.append_hotels));
+        date.setText(data.getDate());
+        bed.setText(data.getBed());
+        adult.setText(data.getAdult());
+        children.setText(data.getChildren());
+        address.setText(data.getAddress());
+
+        String[] list = getList(data);
+        ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<>(this, R.layout.restaurent, list);
+        restaurentList.setAdapter(stringArrayAdapter);
+    }
+
+    private String[] getList(Data data) {
+        String[] restaurentList = null;
+
+        switch (data.getCity()) {
+            case "New York":
+                restaurentList = getResources().getStringArray(R.array.one_restaurent);
+                break;
+            case "New Jersey":
+                restaurentList = getResources().getStringArray(R.array.two_restaurent);
+                break;
+            case "New Hampshire":
+                restaurentList = getResources().getStringArray(R.array.three_restaurent);
+                break;
+            case "Nebraska":
+                restaurentList = getResources().getStringArray(R.array.fourth_restaurent);
+                break;
+            case "Washignton":
+                restaurentList = getResources().getStringArray(R.array.fifth_restaurent);
+                break;
+        }
+
+        return restaurentList;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        List<String> stringList = new ArrayList<>(DataManger.getInstance().getSampleData().keySet());
+
+        final CharSequence[] items = stringList.toArray(new CharSequence[stringList.size()]);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.change_your_hotel);
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int item) {
+                // Do something with the selection
+                DataManger.getInstance().getSampleData().keySet();
+                changeLocation.setText(items[item]);
+
+                // Update the values
+                setValues(DataManger.getInstance().getSampleData().get(items[item].toString()));
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+}
